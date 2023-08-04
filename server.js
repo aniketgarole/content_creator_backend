3523ed56dc832ccb187e3c9b01447266289342ea
aniketgarole <aniketgarole11@gmail.com>
2023-08-04 21:53:54 +0530

    made changes in cors

--- server.js
+++ server.js
@@ -10,17 +10,25 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+// app.use(cors())
 
-app.use((req, res, next) => {
-  //allow access to current url. work for https as well
-  res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
-  res.removeHeader('x-powered-by');
-  //allow access to current method
-  res.setHeader('Access-Control-Allow-Methods',req.method);
-  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+// app.use((req, res, next) => {
+//   //allow access to current url. work for https as well
+//   res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
+//   res.removeHeader('x-powered-by');
+//   //allow access to current method
+//   res.setHeader('Access-Control-Allow-Methods',req.method);
+//   res.setHeader('Access-Control-Allow-Headers','Content-Type');
+//   next();
+// })
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
-})
+});
 
 app.use("/", jokeRouter)
 
