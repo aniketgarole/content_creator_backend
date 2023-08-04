c7cdc69560d6f9e462b6c3f80d541eb670cdd8f8
aniketgarole <aniketgarole11@gmail.com>
2023-08-04 21:51:33 +0530

    made some changes in cors

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use((req, res, next) => {
+  //allow access to current url. work for https as well
+  res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
+  res.removeHeader('x-powered-by');
+  //allow access to current method
+  res.setHeader('Access-Control-Allow-Methods',req.method);
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  next();
+})
+
 app.use("/", jokeRouter)
 
 app.use("/", shayariRouter)
